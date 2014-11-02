0dd6ac1548341da1941d0d0bd3e7ed75913e7a56
Edward Kmett <ekmett@gmail.com>
2014-11-01 21:02:07 -0400

    fix the exponent some more

--- cbits/half.c
+++ cbits/half.c
@@ -1,7 +1,7 @@
 unsigned short hs_floatToHalf (float f) {
   union { float d; unsigned int i; } u = { f };
   int s =  (u.i >> 16) & 0x00008000;
-  int e = ((u.i >> 16) & 0x000000ff) - 112;
+  int e = ((u.i >> 23) & 0x000000ff) - 112;
   int m =          u.i & 0x007fffff;
   if (e <= 0) {
     if (e < -10) return s; /* underflowed */
