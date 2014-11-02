bb58305f3d6612ee5fe7fe69d6906bbd7b21b5de
Edward Kmett <ekmett@gmail.com>
2014-11-01 21:13:51 -0400

    fix up exceptional cases

--- cbits/half.c
+++ cbits/half.c
@@ -1,12 +1,12 @@
 unsigned short hs_floatToHalf (float f) {
   union { float d; unsigned int i; } u = { f };
-  int s =  (u.i >> 16) & 0x00008000;
-  int e = ((u.i >> 23) & 0x000000ff) - 112;
-  int m =          u.i & 0x007fffff;
+  int s =  (u.i >> 16) & 0x8000;
+  int e = ((u.i >> 23) & 0xff) - 112;
+  int m =          u.i & 0x7fffff;
   if (e <= 0) {
     if (e < -10) return s; /* underflowed */
     /* force leading 1 and round */
-    m |= 0x00800000;
+    m |= 0x800000;
     int t = 14 - e;
     int a = (1 << (t - 1)) - 1;
     int b = (m >> t) & 1;
@@ -21,10 +21,10 @@ unsigned short hs_floatToHalf (float f) {
   }
   
   /* round the normalized float */
-  m = m + 0x00000fff + ((m >> 13) & 1);
+  m = m + 0xfff + ((m >> 13) & 1);
 
   /* significand overflow */
-  if (m & 0x00800000) {
+  if (m & 0x800000) {
      m =  0;
      e += 1;
   }
@@ -35,29 +35,28 @@ unsigned short hs_floatToHalf (float f) {
   return s | (e << 10) | (m >> 13);
 }
 
-
-float hs_halfToFloat (unsigned short c) {
-  int s = (c >> 15) & 0x0001;
-  int e = (c >> 10) & 0x001f;
-  int m =         c & 0x03ff;
+int hs_halfToFloatRep (unsigned short c) {
+  int s = (c >> 15) & 0x001;
+  int e = (c >> 10) & 0x01f;
+  int m =         c & 0x3ff;
   if (e == 0) {
     if (m == 0) /* +/- 0 */ return s << 31;
-    else {
-      /* denormalized */
-      while (!(m & 0x00000400)) {
-        m <<= 1;
-        e -=  1;
-      }
-      e += 1;
-      m &= ~0x00000400;
+    /* denormalized, renormalize it */
+    while (!(m & 0x400)) {
+      m <<= 1;
+      e -=  1;
     }
-  } else if (e == 31) {
-    if (m == 0)  /* +/- infinity */ return (s << 31) | 0x7f800000;
-    else return (s << 31) | 0x7f800000 | (m << 13);
-  }  
+    e += 1;
+    m &= ~0x400;
+  } else if (e == 31) return (s << 31) | 0x7f800000 | (m << 13); /* NaN or +/- infinity */
   e += 112;
   m <<= 13;
+  return (s << 31) | (e << 23) | m;
+}
+
+float hs_halfToFloat (unsigned short c) {
   union { float d; unsigned int i; } u;
-  u.i = (s << 31) | (e << 23) | m;
+  u.i = hs_halfToFloatRep(c);
   return u.d;
 }
+
