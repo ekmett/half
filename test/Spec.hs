3001332ba4f685a42a378fb6bbdea42936c0c5e8
ARATA Mizuki <minorinoki@gmail.com>
2020-02-06 20:58:20 +0900

    Add some tests for isInfinite

--- test/Spec.hs
+++ test/Spec.hs
@@ -31,3 +31,15 @@ main = hspec $ do
       property $ \w -> not (isNaN w) ==> fromHalf (toHalf $ fromHalf w) === fromHalf w
     it "idempotence 2" $ do
       property $ \w -> toHalf (fromHalf $ toHalf w) === toHalf w
+
+  describe "isInfinite" $ do
+    it "should be equivalent to \\x -> x == POS_INF || x == NEG_INF" $
+      property $ \x -> isInfinite x === (x == POS_INF || x == NEG_INF)
+    it "should return True on POS_INF" $
+      isInfinite POS_INF `shouldBe` True
+    it "should return True on NEG_INF" $
+      isInfinite NEG_INF `shouldBe` True
+    it "should return false on QNaN" $
+      isInfinite QNaN `shouldBe` False
+    it "should return false on SNaN" $
+      isInfinite SNaN `shouldBe` False
